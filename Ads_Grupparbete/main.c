1f5ae5260aa64bcb07db556c7d3f2a0cb340a564
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2020-11-09 13:39:38 +0100

    no messages

--- Ads_Grupparbete/main.c
+++ Ads_Grupparbete/main.c
@@ -14,7 +14,7 @@
 typedef struct
 {
     char name[100];
-    char message[100];
+//    char message[100];
     int allAds;
     int tokens;
     int amountAds;
@@ -84,35 +84,35 @@ int main()
 {
     ADS a;
     strcpy(a.name, "IoT-20");
-    strcpy(a.message, "hejhej");
+//    strcpy(a.message, "hejhej");
     a.tokens = 1000;
     a.start = 0;
     a.stop = 1000;
     
     ADS a2;
     strcpy(a2.name, "Svarte Petters Svartbyggen");
-    strcpy(a2.message, "hejhej");
+//    strcpy(a2.message, "hejhej");
     a2.tokens = 1500;
     a.start = 1001;
     a.stop = 2501;
     
     ADS a3;
     strcpy(a3.name, "Farmor Ankas Pajer AB");
-    strcpy(a3.message, "hejhej");
+//    strcpy(a3.message, "hejhej");
     a3.tokens = 3000;
     a.start = 5502;
     a.stop = 8502;
     
     ADS a4;
     strcpy(a4.name, "Långbens detektivbyrå");
-    strcpy(a4.message, "hejhej");
+//    strcpy(a4.message, "hejhej");
     a4.tokens = 4000;
     a.start = 8503;
     a.stop = 15504;
     
     ADS a5;
     strcpy(a5.name, "Hederlige Harrys Bilar");
-    strcpy(a5.message, "hejhej");
+//    strcpy(a5.message, "hejhej");
     a5.tokens = 5000;
     a.start = 15504;
     a.stop = 20505;
