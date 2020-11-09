37faa5fa5db46bc7ff6c8b568d4cd3bc66df83fc
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2020-11-09 16:05:12 +0100

    latest

--- Ads_Grupparbete/main.c
+++ Ads_Grupparbete/main.c
@@ -10,11 +10,11 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
+srand(10000);
+
 typedef struct
 {
-    char name[100];
-//    char message[100];
+    int name;
     int allAds;
     int tokens;
     int start;
@@ -28,7 +28,7 @@ void RunOneOfIot()
 }
 void RunOneOfSvarteP()
 {
-    //funktion
+    
     
 }
 void RunOneOfFarmor()
@@ -48,27 +48,27 @@ void RunOneOfHarry()
 }
 void HandleWinning(ADS winning)
 {
-    if (!strcmp(winning.name, "IoT-20"))
+    if (winning.name ==1)
     {
         RunOneOfIot();
         
     }
-    if (!strcmp(winning.name, "Svarte Petters Svartbyggen"))
+    else if (winning.name ==2)
     {
         RunOneOfSvarteP();
 
     }
-    if (!strcmp(winning.name, "Farmor Ankas Pajer AB"))
+    else if (winning.name ==3)
     {
         RunOneOfFarmor();
 
     }
-    if (!strcmp(winning.name, "Långbens detektivbyrå"))
+    else if (winning.name ==4)
     {
         RunOneOfLångben();
 
     }
-    if (!strcmp(winning.name, "Hederlige Harrys Bilar"))
+    else if (winning.name ==5)
     {
         RunOneOfHarry();
 
@@ -82,36 +82,31 @@ void HandleWinning(ADS winning)
 int main()
 {
     ADS a;
-    strcpy(a.name, "IoT-20");
-//    strcpy(a.message, "hejhej");
+    a.name = 1;
     a.tokens = 1000;
     a.start = 0;
     a.stop = 1000;
     
     ADS a2;
-    strcpy(a2.name, "Svarte Petters Svartbyggen");
-//    strcpy(a2.message, "hejhej");
+    a2.name =2;
     a2.tokens = 1500;
     a.start = 1001;
     a.stop = 2501;
     
     ADS a3;
-    strcpy(a3.name, "Farmor Ankas Pajer AB");
-//    strcpy(a3.message, "hejhej");
+    a3.name =3;
     a3.tokens = 3000;
     a.start = 5502;
     a.stop = 8502;
     
     ADS a4;
-    strcpy(a4.name, "Långbens detektivbyrå");
-//    strcpy(a4.message, "hejhej");
+    a4.name =4;
     a4.tokens = 4000;
     a.start = 8503;
     a.stop = 15504;
     
     ADS a5;
-    strcpy(a5.name, "Hederlige Harrys Bilar");
-//    strcpy(a5.message, "hejhej");
+    a5.name =5;
     a5.tokens = 5000;
     a.start = 15504;
     a.stop = 20505;
@@ -124,36 +119,31 @@ int main()
         customers[4] = a5;
         int summatokens = 0;
     for (int i = 0; i < 5; i++)
-            summatokens += customers[i].tokens;
-        
-        srand(time(NULL));
-        int r = rand() % summatokens;
+    {
+        summatokens += customers[i].tokens;
+    }
     ADS winning;
+    int r = rand() % summatokens;
     if (r >= a.start && r <a.stop)
     {
         winning = a;
     }
-    if (r >= a2.start && r <a2.stop)
+    else if (r >= a2.start && r <a2.stop)
     {
         winning = a2;
     }
-    if (r >= a3.start && r <a3.stop)
+    else if (r >= a3.start && r <a3.stop)
     {
         winning = a3;
     }
-    if (r >= a4.start && r <a4.stop)
+    else if (r >= a4.start && r <a4.stop)
     {
         winning = a4;
     }
-    if (r >= a5.start && r <a5.stop)
-    {
+    else {
         winning = a5;
     }
-
         HandleWinning(winning);
- 
-        
-  
         return 0;
 }
 
