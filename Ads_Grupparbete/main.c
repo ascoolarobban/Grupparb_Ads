779cc98bc0c961cd6aea3269ccd98757cc12bab6
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2020-11-09 13:38:21 +0100

    Ny main

--- Ads_Grupparbete/main.c
+++ Ads_Grupparbete/main.c
@@ -1,9 +1,9 @@
 //
 //  main.c
-//  STEFAN GENOMGÅNG
+// Grupparbete_Reklamskylt
 //
 //  Created by Robin Ellingsen on 2020-11-09.
-//inge malloc på arduino
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -24,11 +24,27 @@ typedef struct
 }ADS;
 void RunOneOfIot()
 {
-    //Kanske slumpa ??
-    //if(----)
-    //    Blink("Hello")
-    //if(----)
-    //    Scroll("There")
+    //funktion
+    
+}
+void RunOneOfSvarteP()
+{
+    //funktion
+    
+}
+void RunOneOfFarmor()
+{
+    //funktion
+    
+}
+void RunOneOfLångben()
+{
+    //funktion
+    
+}
+void RunOneOfHarry()
+{
+    //funktion
     
 }
 void HandleWinning(ADS winning)
@@ -38,15 +54,27 @@ void HandleWinning(ADS winning)
         RunOneOfIot();
         
     }
+    if (!strcmp(winning.name, "Svarte Petters Svartbyggen"))
+    {
+        RunOneOfSvarteP();
 
-//    if (!strcmp(winning.name, "Svarte Petters Svartbyggen"))
-//        RunOneOfSvarteP();
-//    if (!strcmp(winning.name, "Farmor Ankas Pajer AB"))
-//        RunOneOfFarmor();
-//    if (!strcmp(winning.name, "Långbens detektivbyrå"))
-//        RunOneOfLångben();
-//    if (!strcmp(winning.name, "Hederlige Harrys Bilar"))
-//        RunOneOfHarry();
+    }
+    if (!strcmp(winning.name, "Farmor Ankas Pajer AB"))
+    {
+        RunOneOfFarmor();
+
+    }
+    if (!strcmp(winning.name, "Långbens detektivbyrå"))
+    {
+        RunOneOfLångben();
+
+    }
+    if (!strcmp(winning.name, "Hederlige Harrys Bilar"))
+    {
+        RunOneOfHarry();
+
+    }
+
 }
 
 
@@ -57,35 +85,35 @@ int main()
     ADS a;
     strcpy(a.name, "IoT-20");
     strcpy(a.message, "hejhej");
-    a.tokens = 1000; //1501-1600
+    a.tokens = 1000;
     a.start = 0;
     a.stop = 1000;
     
     ADS a2;
     strcpy(a2.name, "Svarte Petters Svartbyggen");
     strcpy(a2.message, "hejhej");
-    a2.tokens = 1500; //1501-1600
+    a2.tokens = 1500;
     a.start = 1001;
     a.stop = 2501;
     
     ADS a3;
     strcpy(a3.name, "Farmor Ankas Pajer AB");
     strcpy(a3.message, "hejhej");
-    a3.tokens = 3000; //1501-1600
+    a3.tokens = 3000;
     a.start = 5502;
     a.stop = 8502;
     
     ADS a4;
     strcpy(a4.name, "Långbens detektivbyrå");
     strcpy(a4.message, "hejhej");
-    a4.tokens = 4000; //1501-1600
+    a4.tokens = 4000;
     a.start = 8503;
     a.stop = 15504;
     
     ADS a5;
     strcpy(a5.name, "Hederlige Harrys Bilar");
     strcpy(a5.message, "hejhej");
-    a5.tokens = 5000; //1501-1600
+    a5.tokens = 5000;
     a.start = 15504;
     a.stop = 20505;
     
@@ -96,25 +124,41 @@ int main()
         customers[3] = a4;
         customers[4] = a5;
         int summatokens = 0;
-    for (int i = 0; i < 4; i++)
-            summatokens += customers[i].allAds;
+    for (int i = 0; i < 5; i++)
+            summatokens += customers[i].tokens;
         
         srand(time(NULL));
         int r = rand() % summatokens;
+    ADS winning;
+    if (r >= a.start && r <a.stop)
+    {
+        winning = a;
+    }
+    if (r >= a2.start && r <a2.stop)
+    {
+        winning = a2;
+    }
+    if (r >= a3.start && r <a3.stop)
+    {
+        winning = a3;
+    }
+    if (r >= a4.start && r <a4.stop)
+    {
+        winning = a4;
+    }
+    if (r >= a5.start && r <a5.stop)
+    {
+        winning = a5;
+    }
 
-        //Loopa för att se vem som innehar den lotten
-
-        //När vi fått fram vem som har lotten
-        int i = 2; //Låt oss säga att det blev 2
-        ADS winning = customers[i];
         HandleWinning(winning);
+ 
         
-        
-        //Huvudmeny(&state);
+  
         return 0;
 }
 
-//Senap
+
 
 
 
